01a9c377615ca73c89646a8aac0442643e69881b
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-16 20:00:00 +0530

    Books: views and urls to List books

--- Chapter02/library/books/views.py
+++ Chapter02/library/books/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'books/book_list.html'
