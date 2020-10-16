6c7d62645542b0b2683ad309c9a6da02eb650c61
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-16 23:38:19 +0530

    Todos: Endpoints for all and a single todo

--- Chapter03/todo/backend/todo_project/settings.py
+++ Chapter03/todo/backend/todo_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+
     # Local
     'todos.apps.TodosConfig',
 ]
@@ -122,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Customize
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
