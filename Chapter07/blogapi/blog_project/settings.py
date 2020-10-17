ed3eed5ee6a34cd2a5c1f975f1f7faf61234cab6
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-18 00:24:01 +0530

    Signup: dj-rest-auth and django-allauth

--- Chapter07/blogapi/blog_project/settings.py
+++ Chapter07/blogapi/blog_project/settings.py
@@ -38,11 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd party
     'rest_framework',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth',
+    'dj_rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
@@ -140,3 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
