4dc4ddc5e6069bf6ec9ce518d2c1a03bae06c9a8
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-17 22:54:24 +0530

    Use TokenAuthentication

--- Chapter07/blogapi/blog_project/settings.py
+++ Chapter07/blogapi/blog_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # 3rd party
     'rest_framework',
+    'rest_framework.authtoken',
 
     # Local
     'posts.apps.PostsConfig',
@@ -132,5 +133,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
