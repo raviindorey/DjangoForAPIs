d9ca9d35f4c63bce2237b6d27f9138ccdb4cba54
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-18 02:11:56 +0530

    Docs for Blog API

--- Chapter09/blogapi/blog_project/urls.py
+++ Chapter09/blogapi/blog_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +28,9 @@ urlpatterns = [
     path('api/v1/rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
-    path('schema/', get_schema_view(title='Blog API')),
+    path('docs/', include_docs_urls(
+        title=API_TITLE,
+        description=API_DESCRIPTION
+    )),
+    path('schema/', get_schema_view(title=API_TITLE)),
 ]
